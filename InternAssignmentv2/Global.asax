2a2a6211b9631630addf37afff6f85c544d00cb5
NewUser <NewUser@WAZRP>
2018-07-20 10:14:14 +0200

    Add project files.

---
+++ InternAssignmentv2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InternAssignmentv2.MvcApplication" Language="C#" %>
---
+++ InternAssignmentv2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InternAssignmentv2.Startup))]
+namespace InternAssignmentv2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
