ee90c6c8d56213c6fa1a5db1b0f0ce7ca0890be2
Steven Knoop <s.a.knoop@gmail.com>
2018-07-20 15:06:23 +0200

    Functioning data access

--- InternAssignmentv2/Migrations/Configuration.cs
+++ InternAssignmentv2/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace InternAssignmentv2.Migrations
 {
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -9,24 +10,14 @@ namespace InternAssignmentv2.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             ContextKey = "InternAssignmentv2.Models.InternAssignmentv2Db";
         }
 
         protected override void Seed(InternAssignmentv2.Models.InternAssignmentv2Db context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.Users.AddOrUpdate(r => r.Email,
+                new User { Name = "Andrew", Email = "trudgedy@gmail.com", Password = "YouCantKnow" });
         }
     }
 }
